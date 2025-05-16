e95b2416ff6e2c46a6a099582aa20753b221da32
AnastasiaKilko <anastasia.v.kilko@gmail.com>
2025-05-16 18:00:40 +0700

    logged in as a testerEmployer

--- cypress/e2e/create_new_need.cy.js
+++ cypress/e2e/create_new_need.cy.js
@@ -1,3 +1,8 @@
 describe('Создание новой потребности работодателем', () => {
-
+    it('Позитивный тест: Создание новой потребности работодателем', () => {
+        cy.visit('https://dev.profteam.su/login')
+        cy.get('input[type="text"]').type('testerEmployer')
+        cy.get('input[type="password"]').type('Password1')
+        cy.get('div[class="login-form__button"] button[type="submit"]').click()
+    })
 })
