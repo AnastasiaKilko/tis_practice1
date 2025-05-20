e2f2a747a80a1495d8e384f47fefdf0b6f1e9c55
AnastasiaKilko <anastasia.v.kilko@gmail.com>
2025-05-21 00:58:12 +0700

    student's response

---
+++ cypress/e2e/student_response.cy.js
@@ -0,0 +1,13 @@
+describe('Отклик студента на потребность', () => {
+    it('Позитивный тест - отклик студента', () => {
+
+        cy.visit('https://dev.profteam.su/login')
+        cy.get('input[type=text]').type('nastyaStudent');
+        cy.get('input[type=password]').type('Password1');
+        cy.get('button[type=submit]').eq(2).click();
+        cy.url().should('include', '/account/main');
+
+        cy.get('header nav a.header__item span').contains('Потребности').click();
+        cy.get('button.vacancy-page-card__button').first().click();
+    })
+})
